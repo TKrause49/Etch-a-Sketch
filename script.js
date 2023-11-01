bd452d7608f08c92221a83b64d24976172c183a8
Trey Krause <tjkrause49@gmail.com>
2023-11-01 10:17:04 -0400

    Add event function to let user change size of grid
    Created button the fires event on click. Allows user to pick a number
    and generate a new grid at the chosen side. The number must be between
    0 and 100 to change the grid or the function exits.

--- script.js
+++ script.js
@@ -1,22 +1,30 @@
 const container = document.querySelector('#container');
-
+const button = document.querySelector('button');
 
 function makeSquares (x) {
     for (let i = 0; i < (x*x); i++){
         const square = document.createElement('div');
         const size = (100/x);
         square.classList.add('square');
-        square.style.width = `${size}%`
-        square.style.height = `${size}%`
+        square.style.width = `${size}%`;
+        square.style.height = `${size}%`;
         container.appendChild(square);
-    }
-}
 
-// let x = 16;
-// let size = (100/x);
-
-// for (let i = 0; i < (x*x); i++) {
-//     makeSquares();
-// }
+        square.addEventListener('mouseover', () => {
+            square.style.backgroundColor = 'green';
+        })
+    };
+};
 
 makeSquares(16);
+
+button.addEventListener('click', () => {
+    const size = prompt('Choose your size:');
+    if (size >= 100 || size <= 0){
+        return alert('Please choose a number greater than 0 or less than 100');
+    }
+    while (container.firstChild){
+        container.removeChild(container.firstChild)
+    };
+    makeSquares(size);
+});
